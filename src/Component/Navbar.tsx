b65a31d695f0d2a9582b1746ba10faf6cbe519a3
Mandira Basnet <mandeerab20@gmail.com>
2023-08-21 14:48:19 +0545

    feat: Navbar build

---
+++ src/Component/Navbar.tsx
@@ -0,0 +1,10 @@
+import { HStack, Image } from "@chakra-ui/react";
+
+export const Navbar = () => {
+    return (
+        <HStack> 
+            <Image boxSize="60px"
+             src="http://storage.googleapis.com/ssg-dev-backend-template-bucket/image/acde2c67-443b-4387-9a90-2605ac4e6781" />
+        </HStack>
+    )
+};
